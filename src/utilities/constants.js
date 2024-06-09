7456f437367bd9341cbb185cbd050e90d64f373a
prasanna kumar <prasannakumar8332@gmail.com>
2024-06-09 23:27:21 +0530

    changed api keys

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -5,11 +5,11 @@ export const NETFLIX_BACKGROUND =
   "https://assets.nflxext.com/ffe/siteui/vlv3/7ca5b7c7-20aa-42a8-a278-f801b0d65fa1/fb548c0a-8582-43c5-9fba-cd98bf27452f/IN-en-20240326-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
 
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDG_CZpFQSHFP2QJmmFhJy8m3k9scCete8",
-    authDomain: "netflix-clone-15064.firebaseapp.com",
-    projectId: "netflix-clone-15064",
-    storageBucket: "netflix-clone-15064.appspot.com",
-    messagingSenderId: "741473668809",
-    appId: "1:741473668809:web:2ba7b95edfacef70aec718",
-    measurementId: "G-T5WXLQ327L"
-  };
+  apiKey: "AIzaSyAtPTlmHGLjnE2YIyk_9MCceVWitvhzFtc",
+  authDomain: "netflix-aeea8.firebaseapp.com",
+  projectId: "netflix-aeea8",
+  storageBucket: "netflix-aeea8.appspot.com",
+  messagingSenderId: "896540507182",
+  appId: "1:896540507182:web:be500ef136c220201873bf",
+  measurementId: "G-YFXMWD9WH2",
+};
