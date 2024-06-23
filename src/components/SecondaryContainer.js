f924f8eecf2a2ccbe3c624998acb2bb3e49e5c61
prasanna kumar <prasannakumar8332@gmail.com>
2024-06-23 08:44:31 +0530

    added more container

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -5,19 +5,70 @@ import { useSelector } from "react-redux";
 const SecondaryContainer = () => {
   const nowPlaying = useSelector((state) => state.movie.nowPlaying);
   return (
-    <div className="flex overflow-x-scroll gap-5 my-3 bg-black">
-      {nowPlaying &&
-        nowPlaying.map((movie) => {
-          return (
-            <div className="card-dy">
-              <MovieCard
-                title={movie.title}
-                description={movie.overview}
-                imagePath={movie.poster_path}
-              />
-            </div>
-          );
-        })}
+    <div className="flex flex-col">
+      <h3 className="text-white text-3xl my-1">Now Playing</h3>
+      <div className="flex overflow-x-scroll gap-5 my-3 bg-black overflow-hidden">
+        {nowPlaying &&
+          nowPlaying.map((movie) => {
+            return (
+              <div className="card-dy">
+                <MovieCard
+                  title={movie.title}
+                  description={movie.overview}
+                  imagePath={movie.poster_path}
+                />
+              </div>
+            );
+          })}
+      </div>
+      <h3 className="text-white text-3xl my-1">Now Playing</h3>
+
+      <div className="flex overflow-x-scroll gap-5 my-3 bg-black overflow-hidden">
+        {nowPlaying &&
+          nowPlaying.map((movie) => {
+            return (
+              <div className="card-dy">
+                <MovieCard
+                  title={movie.title}
+                  description={movie.overview}
+                  imagePath={movie.poster_path}
+                />
+              </div>
+            );
+          })}
+      </div>
+      <h3 className="text-white text-3xl my-1">Now Playing</h3>
+
+      <div className="flex overflow-x-scroll gap-5 my-3 bg-black overflow-hidden">
+        {nowPlaying &&
+          nowPlaying.map((movie) => {
+            return (
+              <div className="card-dy">
+                <MovieCard
+                  title={movie.title}
+                  description={movie.overview}
+                  imagePath={movie.poster_path}
+                />
+              </div>
+            );
+          })}
+      </div>
+      <h3 className="text-white text-3xl my-1">Now Playing</h3>
+
+      <div className="flex overflow-x-scroll gap-5 my-3 bg-black overflow-hidden">
+        {nowPlaying &&
+          nowPlaying.map((movie) => {
+            return (
+              <div className="card-dy">
+                <MovieCard
+                  title={movie.title}
+                  description={movie.overview}
+                  imagePath={movie.poster_path}
+                />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
